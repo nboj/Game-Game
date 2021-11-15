2bf80ad4b5853d1d33d4034d3ca4265cab7367b9
nboj <christian.auman@outlook.com>
2021-11-15 18:59:54 -0500

    Update Enemy Movement

--- Order of the Goo/Assets/Scripts/Core/AIController.cs
+++ Order of the Goo/Assets/Scripts/Core/AIController.cs
@@ -1,9 +1,30 @@
 using UnityEngine; 
+using RPG.Control;
 
 namespace RPG.Core {
-    public class AIController : MonoBehaviour {
-        private void Start() {
+    public class AIController : MonoBehaviour { 
+        [SerializeField] EnemySO enemy;
+        private new Rigidbody2D rigidbody;
+        private PlayerController player;
 
+        private void Start() {
+            player = FindObjectOfType<PlayerController>();
+            rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update() {
+            float playerDistance = Vector2.Distance(player.transform.position, transform.position);
+            if (playerDistance <= enemy.ChaseDistance) { 
+                Vector2 velocity = (player.transform.position - transform.position).normalized;
+                rigidbody.velocity = velocity * enemy.EnemySpeed * Time.deltaTime;
+            } else {
+                rigidbody.velocity = Vector2.zero;
+            }
+        }
+
+        private void OnDrawGizmos() { 
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, enemy.ChaseDistance);
         }
     }
 }
