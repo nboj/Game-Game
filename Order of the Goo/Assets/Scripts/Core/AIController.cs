12156b85b1d2e959abd09740d00118df728fd608
Christian <christian.auman@outlook.com>
2021-11-16 14:11:52 -0500

    Enemy movement/combat

--- Order of the Goo/Assets/Scripts/Core/AIController.cs
+++ Order of the Goo/Assets/Scripts/Core/AIController.cs
@@ -1,30 +1,60 @@
 using UnityEngine; 
 using RPG.Control;
+using RPG.Combat;
 
 namespace RPG.Core {
     public class AIController : MonoBehaviour { 
         [SerializeField] EnemySO enemy;
+        [SerializeField] bool willStopOnAttack = false;
+        private int selectedWeaponIndex;
+        private Fighter fighter;
         private new Rigidbody2D rigidbody;
         private PlayerController player;
 
         private void Start() {
             player = FindObjectOfType<PlayerController>();
             rigidbody = GetComponent<Rigidbody2D>();
+            fighter = GetComponent<Fighter>();
+            selectedWeaponIndex = 0;
         }
 
         private void Update() {
             float playerDistance = Vector2.Distance(player.transform.position, transform.position);
-            if (playerDistance <= enemy.ChaseDistance) { 
-                Vector2 velocity = (player.transform.position - transform.position).normalized;
-                rigidbody.velocity = velocity * enemy.EnemySpeed * Time.deltaTime;
+            if (playerDistance <= enemy.ChaseDistance && playerDistance > enemy.AttackDistance) { 
+                MoveToward(player.transform.position);
+            } else if (playerDistance <= enemy.AttackDistance && playerDistance > enemy.RepelDistance) {
+                if (willStopOnAttack) {
+                    StopMoving();
+                } 
+                fighter.FireWeapon(player.transform.position, selectedWeaponIndex);
+            } else if (playerDistance <= enemy.RepelDistance) {
+                MoveAway(player.transform.position);
             } else {
-                rigidbody.velocity = Vector2.zero;
+                StopMoving();
             }
         }
 
+        private void StopMoving() {
+            rigidbody.velocity = Vector2.zero;
+        }
+
+        private void MoveToward(Vector3 position) {
+            Vector2 velocity = (position - transform.position).normalized;
+            rigidbody.velocity = velocity * enemy.EnemySpeed * Time.deltaTime;
+        }
+
+        private void MoveAway(Vector3 position) { 
+            Vector2 velocity = (transform.position - position).normalized;
+            rigidbody.velocity = velocity * enemy.EnemySpeed * Time.deltaTime;
+        }
+
         private void OnDrawGizmos() { 
-            Gizmos.color = Color.red;
+            Gizmos.color = Color.yellow;
             Gizmos.DrawWireSphere(transform.position, enemy.ChaseDistance);
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, enemy.AttackDistance);
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(transform.position, enemy.RepelDistance);
         }
     }
 }
--- Order of the Goo/Assets/Scripts/Core/EnemySO.cs
+++ Order of the Goo/Assets/Scripts/Core/EnemySO.cs
@@ -5,9 +5,11 @@ public class EnemySO : ScriptableObject {
         [SerializeField] float chaseDistance = 10f;
         [SerializeField] float attackDistance = 1f;
         [SerializeField] float enemySpeed = 2000f;
+        [SerializeField] float repelDistance = 0.5f;
         [SerializeField] Sprite enemySprite;
         public float ChaseDistance { get => chaseDistance; }
         public float AttackDistance { get => attackDistance; }
         public float EnemySpeed { get => enemySpeed; }
+        public float RepelDistance { get => repelDistance; }
         public Sprite EnemySprite { get => enemySprite; }
 }
