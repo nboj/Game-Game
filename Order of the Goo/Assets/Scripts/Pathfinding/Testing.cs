8dc4ad1d1e219ce79da939336e710e804bf76663
nboj <christian.auman@outlook.com>
2021-11-17 10:55:07 -0500

    Added Grid class and tester

---
+++ Order of the Goo/Assets/Scripts/Pathfinding/Testing.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+namespace Pathfinding {
+    public class Testing : MonoBehaviour {
+        private void Start() {
+            PF_Grid grid = new PF_Grid(4, 2);
+        }
+    }
+}
